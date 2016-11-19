60a84d0c53b024267265b3ffe8338d64d0d5f2df
SilvrDuck <thibault.viglino@epfl.ch>
2016-11-19 21:50:13 +0100

    Dernier commit avant dodo.

--- site/lib/lib.js
+++ site/lib/lib.js
@@ -3,6 +3,8 @@ function getTemperature(uuid, minor)
     var url = "http://lauzhack.ael.li/events/uuid/" + uuid + "/type/temperature/minor/" + minor + "/date/1000000000/2479563000";
     var url2 = "http://lauzhack.ael.li/events/uuid/EMSENSORDEVICEFORMAT00000001/type/temperature/minor/15/date/1000000000/2479563000";
     var cliclou = $.ajax({
+        type: 'GET',
+        crossDomain: true,
         url: url2,
         headers: {
         'token': 'saEbYNtHbxZ6ThHE'
