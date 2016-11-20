654a0e7980f793606a42e87bed0c51a52dc523ac
valentin <bouda.88@gmail.com>
2016-11-20 11:21:44 +0100

    modif getTemp

--- site/lib/lib.js
+++ site/lib/lib.js
@@ -8,14 +8,14 @@ function getTemperature(uuid, minor)
     // google-chrome --disable-web-security --user-data-dir
     var url = "http://lauzhack.ael.li/events/uuid/" + uuid + "/type/temperature/minor/" + minor + "/date/1000000000/2479563000";
     var url2 = "http://lauzhack.ael.li/events/uuid/EMSENSORDEVICEFORMAT00000001/type/temperature/minor/15/date/1000000000/2479563000";
-    var cliclou = $.ajax({
+    var cliclou_json = $.ajax({
         type: 'GET',
         url: url2,
         headers: {
         'token': 'saEbYNtHbxZ6ThHE',
         }
     });
-    //console.log(cliclou.value);
-    return cliclou.value;
+    
+    console.log(cliclou_json);
+    //return cliclou.value;
 }
-
