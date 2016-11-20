4e8197eb5a8305e652860eff4d4941d0e343cad3
SilvrDuck <thibault.viglino@epfl.ch>
2016-11-20 10:40:10 +0100

    Works with hacked chrome

--- site/lib/lib.js
+++ site/lib/lib.js
@@ -8,10 +8,9 @@ function getTemperature(uuid, minor)
     var url2 = "http://lauzhack.ael.li/events/uuid/EMSENSORDEVICEFORMAT00000001/type/temperature/minor/15/date/1000000000/2479563000";
     var cliclou = $.ajax({
         type: 'GET',
-        crossDomain: true,
         url: url2,
         headers: {
-        'token': 'saEbYNtHbxZ6ThHE'
+        'token': 'saEbYNtHbxZ6ThHE',
         }
     });
     //console.log(cliclou.value);
