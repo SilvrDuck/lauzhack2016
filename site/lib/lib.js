ac03505ad5bd3ce6283c0c5c0a1a53a7fdd5687b
SilvrDuck <thibault.viglino@epfl.ch>
2016-11-20 10:52:41 +0100

    Retry

--- site/lib/lib.js
+++ site/lib/lib.js
@@ -4,6 +4,8 @@ $(function () {
 
 function getTemperature(uuid, minor)
 {
+    // launch browser with
+    // google-chrome --disable-web-security --user-data-dir
     var url = "http://lauzhack.ael.li/events/uuid/" + uuid + "/type/temperature/minor/" + minor + "/date/1000000000/2479563000";
     var url2 = "http://lauzhack.ael.li/events/uuid/EMSENSORDEVICEFORMAT00000001/type/temperature/minor/15/date/1000000000/2479563000";
     var cliclou = $.ajax({
